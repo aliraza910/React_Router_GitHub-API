c3290b7dd5e9f07051b57ef8cd11ff1b87f516f9
Ali Raza <aliraza512joker@gmail.com>
2024-08-28 14:38:01 +0500

    Footer Section Error Resolved

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -78,9 +78,10 @@ export default function Footer() {
         <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-sm text-gray-500 sm:text-center">
-            © 2023
-            <a href="https://hiteshchoudhary.com/" className="hover:underline">
-              hiteshchoudhary
+            © {new Date().getFullYear()}
+            <a htmlHref="https://github.com/aliraza910"  className="hover:underline"
+            target="_blank">
+              Ali Raza
             </a>
             . All Rights Reserved.
           </span>
